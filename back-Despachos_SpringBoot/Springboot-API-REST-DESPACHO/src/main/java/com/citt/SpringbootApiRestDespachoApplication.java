8e825a6791b845faa8b3f1c325a9cba373b3a9f9
Elpeor <kotetorrerami@gmail.com>
2026-05-16 20:35:41 -0400

    prueba de pipeline en despacho

--- back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/SpringbootApiRestDespachoApplication.java
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/java/com/citt/SpringbootApiRestDespachoApplication.java
@@ -8,5 +8,6 @@ public class SpringbootApiRestDespachoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApiRestDespachoApplication.class, args);
+		// este comentario es solo para comprobar el funcionamiento del pipeline de CI/CD
 	}
 }
