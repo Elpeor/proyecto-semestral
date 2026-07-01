1edcd49b4622a50ca8376325706b28e69b2e268d
Elpeor <kotetorrerami@gmail.com>
2026-07-01 11:54:11 -0400

    cambio a tableCompras #14

--- front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
@@ -40,7 +40,7 @@ export const TableCompras = () => {
             <table className="table-fixed">
               <thead>
                 <tr className="py-10">
-                  <th className="pr-10">Orden de hola soy una prueba 13</th>
+                  <th className="pr-10">Orden de hola soy una prueba 14</th>
                   <th className="pr-10">direccion</th>
                   <th className="pr-10">fecha de compra</th>
                   <th className="pr-10">valor total</th>
